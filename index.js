a57d170ad320906109bba3b7751b4afd1493e7e6
Kent Chau <kent@thumbtack.com>
2021-02-17 12:20:57 -0500

    more fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function main() {
-  console.log ('Hello world')
-  console.log ('Let\'s see this how this will be integrated');
+  console.log ("Hello world");
+  console.log ("Let's see this how this will be integrated");
 }
 
 main()
