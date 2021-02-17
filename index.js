35cdd7ffe51d5e0591f595feb119ec9bfe567bcf
Kent Chau <kent@thumbtack.com>
2021-02-17 12:41:56 -0500

    WIP

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function main() {
   console.log ("Hello world");
   console.log ("Let's see this how this will be integrated");
+  const value = true;
+  if (value && 1) {
+    console.log("Lets try this");
+  }
 }
 
 main()
